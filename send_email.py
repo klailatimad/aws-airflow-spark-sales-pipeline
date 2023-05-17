79318177f2ad2c2a7f916b26a9b7537a6ac2991d
Imad Klailat <122483291+klailatimad@users.noreply.github.com>
2023-05-17 17:52:27 -0400

    Create send_email.py

---
+++ send_email.py
@@ -0,0 +1,63 @@
+import boto3
+from botocore.exceptions import ClientError
+
+def send_email():
+    # Replace sender@example.com with your "From" address.
+    # This address must be verified with Amazon SES.
+    SENDER = "email@gmail.com"
+
+    # Replace recipient@example.com with a "To" address. If your account 
+    # is still in the sandbox, this address must be verified.
+    RECIPIENT = "email@gmail.com"
+
+
+    # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
+    AWS_REGION = "us-east-2"
+
+    # The subject line for the email.
+    SUBJECT = "Files missing in S3 bucket"
+
+    # The email body for recipients with non-HTML email clients.
+    BODY_TEXT = ("Files missing in AWS S3 bucket. Please check Snowflake task.")
+
+
+
+    # The character encoding for the email.
+    CHARSET = "UTF-8"
+
+    # Create a new SES resource and specify a region.
+    ## Can be replaced with IAM role instead of typing in access key and secret access key 
+    # client = boto3.client('ses',region_name=AWS_REGION, aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY)
+    client = boto3.client('ses',region_name=AWS_REGION)
+
+
+    # Try to send the email.
+    try:
+        #Provide the contents of the email.
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    RECIPIENT,
+                ],
+            },
+            Message={
+                'Body': {
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': BODY_TEXT,
+                    },
+                },
+                'Subject': {
+                    'Charset': CHARSET,
+                    'Data': SUBJECT,
+                },
+            },
+            Source=SENDER
+
+        )
+    # Display an error if something goes wrong. 
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+        print(response['MessageId'])
